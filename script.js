2ff5b7fec7fc5a95cff93bdbd7a932fdd27efa7e
grealphaBryanLam <96107398+grealphaBryanLam@users.noreply.github.com>
2022-06-23 17:32:26 +0800

    fix race condition in startNotification

--- script.js
+++ script.js
@@ -276,48 +276,49 @@ connectButton.addEventListener("click", async function () {
     
     // GATT operation in progress
     getCharacteristic(SERVICE_UUID, CHARACTERISTIC_FILE_UUID)
-    .then((characteristic) => {
+    .then(async (characteristic) => {
       characteristic.startNotifications()
       .then((characteristic) => {
         time("wait FILE notification");
+      })
+      .catch((error) => {
+        time(error);
       });
       // does the android phone know the notification bit(s) are set?
       characteristic.addEventListener("characteristicvaluechanged", btFileCharacteristicNotifyHandler);
+      return "promise";
     })
-    .catch((error) => {
-      time(error);
-    });
-
-    await delay_ms(NORMAL_DELAY_TIME);
-
-    getCharacteristic(SERVICE_UUID, CHARACTERISTIC_CMD_UUID)
-    .then((characteristic) => {
-      characteristic.startNotifications()
+    .then(async(promise) => {
+      await delay_ms(100);
+      getCharacteristic(SERVICE_UUID, CHARACTERISTIC_CMD_UUID)
       .then((characteristic) => {
-        time("wait CMD notification");
+        characteristic.startNotifications()
+        .then((characteristic) => {
+          time("wait CMD notification");
+          loading_screen.style.display = "none";
+        });
+        characteristic.addEventListener("characteristicvaluechanged", btCmdCharacteristicNotifyHandler);
+      })
+      .catch((error) => {
+        time(error);
         loading_screen.style.display = "none";
       });
-
-      characteristic.addEventListener("characteristicvaluechanged", btCmdCharacteristicNotifyHandler);
+      return "promise";
     })
-    .catch((error) => {
-      time(error);
-      loading_screen.style.display = "none";
-    });
-
-    await delay_ms(NORMAL_DELAY_TIME);
-    /** GATT server is disconnected */
-    getCharacteristic(SERVICE_UUID, CHARACTERISTIC_DEBUG_UUID)
-    .then((characteristic) => {
-      characteristic.startNotifications()
+    .then(async(promise) => {
+      await delay_ms(NORMAL_DELAY_TIME);
+      getCharacteristic(SERVICE_UUID, CHARACTERISTIC_DEBUG_UUID)
       .then((characteristic) => {
-        time("wait DEBUG notification");
-      });
+        characteristic.startNotifications()
+        .then((characteristic) => {
+          time("wait DEBUG notification");
+        });
 
-      characteristic.addEventListener("characteristicvaluechanged", btDebugCharacteristicNotifyHandler);
-    })
-    .catch((error) => {
-      time(error);
+        characteristic.addEventListener("characteristicvaluechanged", btDebugCharacteristicNotifyHandler);
+      })
+      .catch((error) => {
+        time(error);
+      });
     });
   })
   .catch((error) => {
